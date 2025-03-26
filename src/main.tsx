b3ac9b4e15e507e363789e818726ab280356f9fd
ChangwooJ <wjdckddn99@naver.com>
2025-03-26 15:47:00 +0900

    Feat: 라이브러리 및 기타 초기 세팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { theme } from './styles/theme';
+import './index.css';
+import App from './App.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the homepage of the app!</p>
+    </div>
+  );
+};
+
+export default HomePage;
