3a0ebdd4a1af8ebb3b55ee30912838904334c019
ChangwooJ <wjdckddn99@naver.com>
2025-04-04 18:01:38 +0900

    Feat: 필터 관련 Zustand 세팅

--- src/store/useFilterStore.ts
+++ src/store/useFilterStore.ts
@@ -1,23 +1,41 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type FilterStore = {
-  difficulty: number;
-  tages: string;
-  title: string;
-  setSearchTitle: (text: string) => void;
+  difficulty?: number;
+  setDifficulty: (difficult: number) => void;
+  tags?: string;
+  setTags: (tag: string) => void;
+  title?: string;
+  setSearchTitle: (searchKey: string) => void;
 };
 
 export const useFilterStore = create<FilterStore>()(
-  persist (
+  persist(
     (set) => ({
       difficulty: undefined,
-      tages: undefined,
+      tags: undefined,
       title: undefined,
 
       setSearchTitle: (searchKey) => 
-        set(() => ({ title: searchKey }))
+        set(() => ({ title: searchKey })),
 
-    })
+      setDifficulty: (difficult) => 
+        set(() => ({ difficulty: difficult })),
+
+      setTags: (tag) => 
+        set(() => ({ tags: tag })),
+      
+      resetFilters: () =>
+        set(() => ({
+          difficulty: undefined,
+          tags: undefined,
+          title: undefined,
+        })),
+    }),
+    {
+      name: "filter-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
   )
 );
