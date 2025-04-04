a617555679bf8ebb66986da51c5955818bcc26bb
ChangwooJ <wjdckddn99@naver.com>
2025-04-04 14:52:36 +0900

    Fix: 헤더로 인한 스크롤 제거

--- src/layouts/CommonComponents.tsx
+++ src/layouts/CommonComponents.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 
 const Background = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
